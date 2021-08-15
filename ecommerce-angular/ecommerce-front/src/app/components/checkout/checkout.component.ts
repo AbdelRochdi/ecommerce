2745d81887cdd5adf87c695ef5ab8e4bd52b9118
Abdelghafour Rochdi <57940200+AbdelRochdi@users.noreply.github.com>
2021-08-15 15:09:36 +0100

    checkout form

---
+++ ecommerce-angular/ecommerce-front/src/app/components/checkout/checkout.component.ts
@@ -0,0 +1,65 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-checkout',
+  templateUrl: './checkout.component.html',
+  styleUrls: ['./checkout.component.css']
+})
+export class CheckoutComponent implements OnInit {
+
+  checkoutFormGroup?: FormGroup;
+
+  totalPrice: number = 0;
+  totalQuantity: number = 0;
+
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.checkoutFormGroup = this.formBuilder.group({
+      customer: this.formBuilder.group({
+        firstName: [''],
+        lastName: [''],
+        email: [''],
+      }),
+      shippingAddress: this.formBuilder.group({
+        street: [''],
+        city: [''],
+        state: [''],
+        country: [''],
+        zipCode: ['']
+      }),
+      billingAddress: this.formBuilder.group({
+        street: [''],
+        city: [''],
+        state: [''],
+        country: [''],
+        zipCode: ['']
+      }),
+      creditCard: this.formBuilder.group({
+        cardType: [''],
+        nameOnCard: [''],
+        cardNumber: [''],
+        securityCode: [''],
+        expirationMonth: [''],
+        expirationYear: ['']
+      })
+    });
+
+  }
+
+  onSubmit() {
+
+  }
+
+  copyShippingAddressToBillingAddress(event: { target: { checked: any; }; }) {
+
+    if (event.target.checked) {
+      this.checkoutFormGroup?.controls.billingAddress
+        .setValue(this.checkoutFormGroup.controls.shippingAddress.value);
+    } else {
+      this.checkoutFormGroup?.controls.billingAddress.reset()
+    }
+
+  }
+}
